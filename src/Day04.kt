1e96dc328431bf79e1c9534896256375b0ca6691
Daniel Marcotte <dmarcotte@indeed.com>
2022-12-04 10:20:45 -0600

    Day04

---
+++ src/Day04.kt
@@ -0,0 +1,43 @@
+fun main() {
+    fun parseElfPairs(input: List<String>): List<Pair<InclusiveRange, InclusiveRange>> {
+        return input.map { line->
+            val points = line.split(',', '-').map { it.toInt() }
+            check(points.size == 4)
+            Pair(InclusiveRange(points[0], points[1]), InclusiveRange(points[2], points[3]))
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        val elfPairs = parseElfPairs(input)
+        return elfPairs.count { (elf1, elf2) -> elf1.fullyContains(elf2) || elf2.fullyContains(elf1) }
+    }
+
+    fun part2(input: List<String>): Int {
+        val elfPairs = parseElfPairs(input)
+        return elfPairs.count { (elf1, elf2) -> elf1.overlaps(elf2) }
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
+
+data class InclusiveRange(
+    val x: Int,
+    val y: Int
+) {
+    fun fullyContains(other: InclusiveRange): Boolean {
+        return this.x <= other.x && this.y >= other.y
+    }
+
+    fun overlaps(other: InclusiveRange): Boolean {
+        return (this.x >= other.x && this.x <= other.y)
+            || (this.y >= other.x && this.y <= other.y)
+            || (other.x >= this.x && other.x <= this.y)
+            || (other.y >= this.x && other.y <= this.y)
+    }
+}
